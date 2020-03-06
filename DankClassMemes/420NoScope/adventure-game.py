3021f3ff185eddb990af31ac4107eee6dad9e9f4
Peter Boyle <31522669+CaptainCrossbar@users.noreply.github.com>
2020-03-06 13:50:25 -0500

    Add files via upload

---
+++ DankClassMemes/420NoScope/adventure-game.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+"""
+###############################################################################
+#
+# File Name : adventure-game.py
+# Author : CaptainCrossbar
+# Creation Date : 05 - March - 2020
+# Last Modified :
+# Description :
+#
+###############################################################################
+"""
+
+def main():
+    pass
+
+def intro():
+    print('Would you like to go on an adventure?')
+    x = input()
+    if x == 'yes':
+        print('Congrats love! You have chosen wisely. We will begin shortly.')
+    elif x == 'no':
+        print('What you dont want to go on an adventure!? what are you a panzy, mate. You have done mugged me off')
+    elif x == 69:
+        print('Noice. you my good sir or ma am are a person of amazing quailty. Please input either yes or no tho')
+    elif x == 420:
+        print('Ayy blaze it up. Guess what. you just won a random a UA')
+    else:
+        print('I like your style but please input a yes or a no')
+        x = input()
+        if x == 'yes':
+        print()
+        else:
+            print('Youse mugged us off for the last time...')
+
+def adventure1():
+    print('Your first adventure is a recon challenge. In this adventure you will be asked to'
+    'find a certain key word or phrase that will unlock your next adventure. Are you ready?')
+    x = ipnut()
+    if x == 'yes':
+        print()
+    else:
+        print('Whats the matter? You get scared?')
+    pass
+
+def adventure2():
+    pass
+
+def adventure3():
+    pass
+
+def adventure4():
+    pass
+
+def adventure5():
+    pass
+
+def adventure6():
+    pass
+
+def adventure7():
+    pass
+
+def adventure8():
+    pass
+
+def adventure9():
+    pass
+
+def adventure10():
+    pass
+
+def finaldecision():
+    pass
+
+def DioASCiiArt():
+    pass
---
+++ DankClassMemes/420NoScope/test.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+import math
+import random
+"""
+###############################################################################
+#
+# File Name : test.py
+# Author : 2LT PWB
+# Creation Date : 05 - March - 2020
+# Last Modified :
+# Description :
+#
+###############################################################################
+"""
+
+"""
+def main():
+    x = 69
+    y = 69.69
+    z = math.sqrt(176980.0761)
+    a = x + 0
+    b = y + 0
+    c = z + 0
+
+    r = random.randint(1, 1000)
+    if r == 69:
+        print('ayyyyy my mans. that is hot')
+    elif r == 420:
+        print('you won the lottery and jsut scored yourself a, air quotes, random ua')
+    else:
+        print((r), '. better luck next time champ. i just mugged you off bruv')
+
+    """
+    ###
+"""
+    print("Hello World. There are only a few things in life that are key. What"
+    " are they you ask? Well the first is ", a ,". The second is by far the best"
+    " on to remember " , b , ". And the most gamer thing to remember to enrage all"
+    " non-conforming weebs of the world and the most deadly thing to say is " ,
+    c , ". Just remember these three things, you will be naruto running in no time.")
+"""
+    ###
+"""
+    #print(r)
+
+if __name__ == "__main__":
+    main()
+"""
+###########################################################################################
+"""
+
+def memeritv():
+    r = random.randint(1, 1000)
+    if r == 69:
+        print('ayyyyy my mans. that is hot')
+    elif r == 420:
+        print('you won the lottery and jsut scored yourself a, air quotes, random ua')
+    else:
+        print((r), '. better luck next time champ. i just mugged you off bruv')
+    return
+
+"""
+############################################################################################
+def makedeck():
+  suits = ['\u2660','\u2665','\u2666','\u2663']
+  ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+  deck = ['{}{}'.format(rank,suit) for suit in suits for rank in ranks]
+  print(deck)
+  return makedeck
+
+
+
+makedeck()
